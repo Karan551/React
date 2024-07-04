f0fd2892603142113dc4d6dadad5128faa798453
Kartik <iamkaranmishra551@gmail.com>
2024-07-04 09:03:54 +0530

    initialize github profile finder project.

---
+++ GitHub_Profile_Finder/GitHub_profile_finder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+// https://api.github.com/users/karan551
+
+  return (
+    <>
+      
+      <h1 className="text-orange-500 text-3xl text-center bg-gray-300 px-3 py-5 font-semibold">Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
