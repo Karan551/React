05e2bdf505fab665ebdb17fbc28934eafe88cd0e
Kartik <iamkaranmishra551@gmail.com>
2024-07-30 21:32:54 +0530

    context practice

---
+++ todo_with_context/todo-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <main className='min-h-screen bg-black flex flex-col items-center'>
+
+      <h1 className='text-5xl text-orange-500'>Context Provider</h1>
+    </main>
+
+    </>
+  );
+}
+
+export default App;
