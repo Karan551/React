acb1a308b47e1acce9ab6b54f1d7cdea680b6934
Kartik <iamkaranmishra551@gmail.com>
2024-09-09 13:03:59 +0530

    initialize project

---
+++ react_with_dave_gray/gitProject_router/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Router</h1>
+    </>
+  );
+}
+
+export default App;
+
+const About = () => {
+  return <>About Us</>;
+};
+
+const Contact = () => {
+  return <>Contact Us</>;
+}
+
+
