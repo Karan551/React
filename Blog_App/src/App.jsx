c5bcaa24e0d003b9b3f932e7c09e692af2249e5f
Kartik <iamkaranmishra551@gmail.com>
2024-10-26 23:08:15 +0530

    inital setup and enviornment setup finished

---
+++ Blog_App/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { conf } from './conf/config';
+
+function App() {
+  // console.log("This is project endpoint ",conf.appWriteURL)
+  return (
+    <>
+      <h1 className='text-orange-500'>Mega Blog Project</h1>
+    </>
+  );
+}
+
+export default App;
