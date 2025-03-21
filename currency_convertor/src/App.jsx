4ba7c6b7a59601182ef4b81d50c77f043158ea11
Kartik <iamkaranmishra551@gmail.com>
2025-03-21 10:39:38 +0530

    initialize currency converter project with React, Vite, and Tailwind CSS

---
+++ currency_convertor/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main className="text-lg font-semibold">
+    <h1>Currency convertor</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
